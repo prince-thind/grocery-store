83c21c910dcf2d581b68fc2bfa76e019bab2bfe1
Prince-Thind <princethind799@gmail.com>
2021-10-31 17:48:28 +0530

    add missing list route

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/category/create',categoryController.category_create_get);
 router.post('/category/create',categoryController.category_create_post);
 
 router.get('/category/:id',categoryController.category_detail);
+router.get('/categories',categoryController.category_list);
 
 router.get('/category/:id/update',categoryController.category_update_get)
 router.post('/category/:id/update',categoryController.category_update_post);
@@ -27,6 +28,7 @@ router.get('/item/create',itemController.item_create_get);
 router.post('/item/create',itemController.item_create_post);
 
 router.get('/item/:id',itemController.item_detail);
+router.get('/items',itemController.item_list);
 
 router.get('/item/:id/update',itemController.item_update_get)
 router.post('/item/:id/update',itemController.item_update_post);
