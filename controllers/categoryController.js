25e31d2c4fc13dec0baff19e87debe3e2345fefc
Prince-Thind <princethind799@gmail.com>
2021-10-31 17:48:45 +0530

    implement dummy controller functions

---
+++ controllers/categoryController.js
@@ -0,0 +1,28 @@
+exports.category_list = function (req, res, next) {
+  res.send('NOt Impleneted: category list');
+};
+
+exports.category_detail = function (req, res, next) {
+  res.send('NOt Impleneted: category details');
+};
+
+exports.category_create_get = function (req, res, next) {
+  res.send('NOt Impleneted: category create get');
+};
+
+exports.category_create_post = function (req, res, next) {
+  res.send('NOt Impleneted: category create post');
+};
+
+exports.category_update_get = function (req, res, next) {
+  res.send('NOt Impleneted: category update_get');
+};
+exports.category_update_post = function (req, res, next) {
+  res.send('NOt Impleneted: category update_post');
+};
+exports.category_delete_get = function (req, res, next) {
+  res.send('NOt Impleneted: category delete_get');
+};
+exports.category_delete_post = function (req, res, next) {
+  res.send('NOt Impleneted: category delete_post');
+};
---
+++ controllers/itemController.js
@@ -0,0 +1,28 @@
+exports.item_list = function (req, res, next) {
+  res.send('NOt Impleneted: item list');
+};
+
+exports.item_detail = function (req, res, next) {
+    res.send('NOt Impleneted: item details');
+  };
+
+exports.item_create_get = function (req, res, next) {
+  res.send('NOt Impleneted: item create get');
+};
+
+exports.item_create_post = function (req, res, next) {
+  res.send('NOt Impleneted: item create post');
+};
+
+exports.item_update_get = function (req, res, next) {
+  res.send('NOt Impleneted: item update_get');
+};
+exports.item_update_post = function (req, res, next) {
+  res.send('NOt Impleneted: item update_post');
+};
+exports.item_delete_get = function (req, res, next) {
+  res.send('NOt Impleneted: item delete_get');
+};
+exports.item_delete_post = function (req, res, next) {
+  res.send('NOt Impleneted: item delete_post');
+};
