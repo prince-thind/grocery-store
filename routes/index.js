584fe40eb2cb377df463d6e4c3620681485c8d39
Prince-Thind <princethind799@gmail.com>
2021-10-31 19:12:26 +0530

    implement about page

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-const categoryController=require('../controllers/categoryController');
-const itemController=require('../controllers/itemController');
+const categoryController = require('../controllers/categoryController');
+const itemController = require('../controllers/itemController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Foo Groceries' });
+});
+
+//about page
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 //category routes
 
-router.get('/category/create',categoryController.category_create_get);
-router.post('/category/create',categoryController.category_create_post);
+router.get('/category/create', categoryController.category_create_get);
+router.post('/category/create', categoryController.category_create_post);
 
-router.get('/category/:id',categoryController.category_detail);
-router.get('/categories',categoryController.category_list);
+router.get('/category/:id', categoryController.category_detail);
+router.get('/categories', categoryController.category_list);
 
-router.get('/category/:id/update',categoryController.category_update_get)
-router.post('/category/:id/update',categoryController.category_update_post);
+router.get('/category/:id/update', categoryController.category_update_get);
+router.post('/category/:id/update', categoryController.category_update_post);
 
-router.get('/category/:id/delete',categoryController.category_delete_get)
-router.post('/category/:id/delete',categoryController.category_delete_post);
+router.get('/category/:id/delete', categoryController.category_delete_get);
+router.post('/category/:id/delete', categoryController.category_delete_post);
 
 //item routes
-router.get('/item/create',itemController.item_create_get);
-router.post('/item/create',itemController.item_create_post);
+router.get('/item/create', itemController.item_create_get);
+router.post('/item/create', itemController.item_create_post);
 
-router.get('/item/:id',itemController.item_detail);
-router.get('/items',itemController.item_list);
+router.get('/item/:id', itemController.item_detail);
+router.get('/items', itemController.item_list);
 
-router.get('/item/:id/update',itemController.item_update_get)
-router.post('/item/:id/update',itemController.item_update_post);
+router.get('/item/:id/update', itemController.item_update_get);
+router.post('/item/:id/update', itemController.item_update_post);
 
-router.get('/item/:id/delete',itemController.item_delete_get)
-router.post('/item/:id/delete',itemController.item_delete_post);
+router.get('/item/:id/delete', itemController.item_delete_get);
+router.post('/item/:id/delete', itemController.item_delete_post);
 
 module.exports = router;
